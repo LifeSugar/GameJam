21aceb9fd198a017caf3d093b827ed8dd839e1fa
Unknown_X <137373205+Testttt98@users.noreply.github.com>
2025-04-05 16:35:21 +0800

    j

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform playerTransform;
-    public Vector3 offset = new Vector3(0, 0, -10f);
+
+    [Range(0.01f, 1f)]
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset = new Vector3(0, 0, -10);
 
     void LateUpdate()
     {
         if (playerTransform != null)
         {
-            transform.position = playerTransform.position + offset;
+            Vector3 desiredPosition = playerTransform.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
         }
     }
 }
