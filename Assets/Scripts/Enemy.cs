35141c73336a743aed925113ee220f1392567f8d
Unknown_X <137373205+Testttt98@users.noreply.github.com>
2025-04-05 11:20:38 +0800

    Movement (Enemy)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public float speed;
     public bool isAlert;
-    // Start is called before the first frame update
+    public float alertDistance;
+    private Transform player;
+
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform; // Get player coordinates
         isAlert = false;
         speed = 5f;
+        alertDistance = 3f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isAlert)
+        {
+            float distance = Vector2.Distance(transform.position, player.position);
+
+            bool isAbovePlayer = player.position.y < transform.position.y;
+
+            if (distance < alertDistance && isAbovePlayer) // Check if the player is below the enemy and in detection distance
+            {
+                isAlert = true;
+            }
+        }
+        else
+        {
+            ChasePlayer();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) // Check if the enemy has collided with the player
+        {
+            Debug.Log("You Died!");
+        }
+
+        if (other.gameObject.CompareTag("Enemy Blocked!"))
+        {
+            // Enemy blocked by terrain and self destroyed
+            Destroy(gameObject);
+        }
+    }
+
+    void ChasePlayer()
+    {
+        Vector2 enemyPosition = transform.position;
+        Vector2 playerPosition = player.position;
+
+        transform.position = Vector2.MoveTowards(enemyPosition, playerPosition, speed * Time.deltaTime); // Enemy chasing the player
     }
 }
