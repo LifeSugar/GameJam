ff54367d44b1034c2d594cfbc297b1c457a55991
Unknown_X <137373205+Testttt98@users.noreply.github.com>
2025-04-06 15:03:21 +0800

    jjjj

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -15,6 +15,12 @@ namespace ludum
 
         public float hoverAcceleration = 20f;
 
+        // 新增能量相关变量
+        public float maxEnergy = 100f;
+        public float currentEnergy = 100f;
+        public float energyDecaySpeed = 20f;     // 每秒消耗速度
+        public float energyRecoverSpeed = 10f;   // 每秒恢复速度
+
         private bool isDead = false;
 
         public void SetDead()
@@ -29,9 +35,15 @@ namespace ludum
             float horizontal = Input.GetAxis("Horizontal");
             float horizontalMovement = horizontal * playerSpeed;
 
-            if (Input.GetKey(KeyCode.W))
+            bool isHoldingW = Input.GetKey(KeyCode.W);
+
+            if (isHoldingW && currentEnergy > 0f)
             {
                 fallSpeed = Mathf.MoveTowards(fallSpeed, 0f, hoverAcceleration * Time.deltaTime);
+                currentEnergy -= energyDecaySpeed * Time.deltaTime;
+
+                // 防止能量值降至0以下
+                currentEnergy = Mathf.Max(currentEnergy, 0f);
             }
             else if (Input.GetKey(KeyCode.S))
             {
@@ -44,9 +56,17 @@ namespace ludum
                 fallSpeed = Mathf.Min(fallSpeed, maxFallSpeedNoInput);
             }
 
+            // 如果没有按住W，能量逐渐恢复
+            if (!isHoldingW)
+            {
+                currentEnergy += energyRecoverSpeed * Time.deltaTime;
+
+                // 能量不会超过最大值
+                currentEnergy = Mathf.Min(currentEnergy, maxEnergy);
+            }
+
             Vector2 movement = new Vector2(horizontalMovement, -fallSpeed);
             transform.Translate(movement * Time.deltaTime, Space.World);
-
         }
     }
 }
